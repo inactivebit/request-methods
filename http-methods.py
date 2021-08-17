52899df50cd89af14059a730084c3ac91b22c32a
Samet A <alp.samet@me.com>
2021-08-17 15:33:16 +0100

    Update http-methods.py

--- http-methods.py
+++ http-methods.py
@@ -1,1 +1,9 @@
+#Printing out each of the methods that are supported by a given web-server
+
 import requests
+
+list_methods = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'TRACE', 'TEST']
+for method in list_methods:
+  req = requests.request(method, 'https://www.bing.com')
+  print(method, req.status_code, req.reason)
+
